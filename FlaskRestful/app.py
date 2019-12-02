dbd05101e4d82c457a8373a0d2646997ac39d749
ChidinmaKO <kchidinmavictoria@gmail.com>
2019-12-02 03:36:50 +0100

    add JWT and reqparse features to API

--- FlaskRestful/app.py
+++ FlaskRestful/app.py
@@ -1,8 +1,8 @@
 from typing import List, Union, Optional
 
 from flask import Flask, request, render_template
-from flask_restful import Resource, Api
-from flask_jwt import JWT
+from flask_restful import Api, Resource, reqparse
+from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
 
@@ -10,43 +10,69 @@ app = Flask(__name__)
 app.secret_key = "c933ac116227737997d2e27a6d4cd6bf0d3bac640a5ec422c1d203ac58df1487"
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity)
+jwt = JWT(app, authenticate, identity) #creates a new endpoint called '/auth'
 
 
 items = []
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+
+
     # GET /items
     def get(self, name: str)-> Optional[dict]:
         item = next(filter(lambda item: item['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
 
     # POST /item/<string:name>
+    @jwt_required()
     def post(self, name: str)-> List[dict]:
         # if there's already an item matching the name, just return a message
         item = next(filter(lambda item: item['name'] == name, items), None)
         if item:
             return {'message': f"An item with the name {item['name']} already exists!"}, 400
-        data = request.get_json()
+
+        data = Item.parser.parse_args()
+
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return items, 201
+
+    # DELETE /item/<string:name>
+    @jwt_required()  
+    def delete(self, name: str):
+        global items
+        items = list(filter(lambda item: item['name'] != name, items))
+        return {'message': 'Item Deleted'}
+
+    # PUT /item/<string:name>
+    @jwt_required()
+    def put(self, name: str):
+        data = Item.parser.parse_args()
+
+        # check if item exists already
+        item = next(filter(lambda item: item['name'] == name, items), None)
+        if item:
+            item.update(data)
+        else:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        return item
+
+# FIXME: Note that PUT & DELETE are idempotent requests. 
+# A request is Idempotent when the effects of calling it once is the same as calling it several times. 
+# Choose PUT IFF the endpoint must be idempotent and if the URI must be the address to the resource being updated.
 
 
 class ItemList(Resource):
     # GET /items
     def get(self)-> List:
         return {'items': items}
-
-    # DELETE /item/<string:name>  
-    # def delete(self, name: str):
-    #     return
-    #     pass
-
-    # PUT /item/<string:name>
-    # def put(self, name: str):
-    #     return
-    #     pass
 
     
 api.add_resource(Item, '/item/<string:name>') #http://127.0.0.1:3000/item/book
--- FlaskRestful/security.py
+++ FlaskRestful/security.py
@@ -3,7 +3,7 @@ from werkzeug.security import safe_str_cmp
 from user import User
 
 users = [
-    User(1, 'ada', 'asdf')
+    User(1, 'ada', 'asdf'),
     User(2, 'jim', 'lkjh')
 ]
 
@@ -13,7 +13,7 @@ userid_mapping = {user.id: user for user in users}
 
 def authenticate(username, password):
     user = username_mapping.get(username, None)
-    if user and safe_str_cmp(user.password == password):
+    if user and safe_str_cmp(user.password, password):
         return user
 
 def identity(payload):
